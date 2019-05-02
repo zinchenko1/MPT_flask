e4ccf3aec82a1f205572af1f32ed66fb3795c389
zinchenko1 <aswad1909@gmail.com>
2019-05-02 23:28:34 +0300

    js done

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@ jQuery(function($){
   $('a[href*="#"]').on('click.smoothscroll', function( e ){
   var hash= this.hash, _hash   = hash.replace(/#/,''), theHref = $(this).attr('href').replace(/#.*/, '');
   if( theHref && location.href.replace(/#.*/,'') != theHref ) return;
-  var $target = _hash === '' ? $('body') : $(hash + ', a[name="'+ _hash +'"]').first();
+  var $target = _hash === '' ? $('body') : $( hash + ', a[name="'+ _hash +'"]').first();
   if( ! $target.length ) return;
   e.preventDefault();
   $('html, body').stop().animate({ scrollTop: $target.offset().top - 0 }, 1000, 'swing', function(){
@@ -21,7 +21,8 @@ $(document).ready(function() {
         height : $('#height').val(),
         weight : $('#weight').val()
 			},
-			type : 'POST',
+      type : 'POST',
+      timeout: 3000,
 			url : '/app'
 		})
 		.done(function(data) {
@@ -30,17 +31,30 @@ $(document).ready(function() {
         $('#successAlert').hide();
 			}
 			else {
-				$('#successAlert').text(data.name).show();
+        $(".form").css("display", "none");
+				$('#successAlert').text(data.name).fadeIn("slow");
         $('#errorAlert').hide();
 			}
-		});
+    });
+    setTimeout(function() {
+      $("#fff").trigger('reset');
+      $("#form").fadeIn("slow");
+      $('#successAlert').css("display", "none");
+    }, (5555));
 		event.preventDefault();
 	});
 });
 
-$(function() {
-  $("#btn_form").click(function() {
-    $(".form").css("display", "none");
-      // $("#").css("display", "block");
-  });
-});
+// $("button").on("click", function(){
+//   setTimeout(openPopup, 2000);
+//   $(this).prop("disabled", true);
+// // });
+
+// $(function() {
+//   $(".form").fadeIn("slow");
+//   $('#successAlert').css("display", "none");
+// }, 2000);
+
+
+
+
